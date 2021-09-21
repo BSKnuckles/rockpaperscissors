9480c13838e5b8d93ce6e186b398eb46fb9f4051
Michael McNees <michael_mcnees@sweetwater.com>
2021-09-21 15:38:38 -0400

    Better mobile rules layout

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -10,7 +10,10 @@ class RulesModal extends React.Component {
       return (
         <div className={styles.modalWrapper}>
           <dialog className={styles.modal} open={this.props.open}>
-            <header className={styles.modalHeader}>
+            <header className={styles.mobileModalHeader}>
+              <h2 className={styles.heading}>Rules</h2>
+            </header>
+            <header className={styles.desktopModalHeader}>
               <h2 className={styles.heading}>Rules</h2>
               <button className={styles.closeBtn} onClick={this.props.toggleModal}>
                 <span className='sr-only'>Close Rules Modal</span>
@@ -18,6 +21,12 @@ class RulesModal extends React.Component {
               </button>
             </header>
             <img src={rules} alt='Rules' className={styles.image} />
+            <footer className={styles.mobileModalFooter}>
+              <button className={styles.closeBtn} onClick={this.props.toggleModal}>
+                <span className='sr-only'>Close Rules Modal</span>
+                <img src={close} alt='Close Icon' />
+              </button>
+            </footer>
           </dialog>
         </div>
       )
